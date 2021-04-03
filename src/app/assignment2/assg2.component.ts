6f385a0211fe77e8e5e5ab2c1fc7f80e1784a223
srmithra3 <mithrasripathi@gmail.com>
2021-04-03 16:25:15 -0400

    3 files modified

--- src/app/assignment2/assg2.component.ts
+++ src/app/assignment2/assg2.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class Assg2Component {
   username = '';
+   showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
